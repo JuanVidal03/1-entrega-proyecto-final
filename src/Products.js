4912db15aabd9f9c0e36bc1679a9b737b3148cfd
Juan Vidal <juanmvg2003@gmail.com>
2023-01-08 17:03:42 -0500

    fueron creadas las rutas de productos

--- src/Products.js
+++ src/Products.js
@@ -39,18 +39,22 @@ class Products {
             newId = objects[objects.length-1].id+1;
         }
 
-        // se crea el timeStamp
-        const timeStamp = new Date().toLocaleString();
+        // timeStamp producto
+        const timeStamp = Date.now();
+
+        // codigo producto
+        const code = Math.floor(Math.random(1000) * 10000);
+
+        
 
         // creamos el objeto definitivo y lo pusheamos al array contenedor
-        const newObject = { ...objeto, id: newId, timeStamp: timeStamp };
+        const newObject = { ...objeto, id: newId, timeStamp: timeStamp, codigo: code };
         objects.push(newObject);
 
 
         try{
             await fs.promises.writeFile(`${this.path}`, JSON.stringify(objects, null, 2));
             console.log('Producto añadido exitosamente!');
-            // return newId;
 
         } catch(err) {
             throw new Error(`Errar al guardar: ${err}`)
@@ -61,6 +65,9 @@ class Products {
     async getById(id){
 
         try {
+            // inicializo la variable producto
+            let producto;
+
             // obteniendo los objetos
             await this.getAll()
                 .then((res) => {
@@ -68,25 +75,57 @@ class Products {
                     // verificando si el id está en el array
                     if (res.some(product => product.id == id)) {
 
-                        // filtramos y retornamos el productos
-                        const producto = res.find(product => product.id == id);
-                        console.log(producto);
-                        return producto;
+                        // encontramos el producto
+                        producto = res.find(product => product.id == id);
 
                     } else {
-                        return 'El id de producto no existe.'
+                        producto = 'El id de producto no existe.';
                     }
 
 
                 }).catch((err) => {
                     console.log(`Ha ocurirdo un error al obtener la data: ${err}.`);
                 })
+            
+            // este resultado se va a renderizar
+            return producto;
 
         } catch (error) {
             console.log(`Error al obtener el id del producto: ${error}.`);
         }
     }
 
+
+    // eliminaod producto por id
+    async deleteById(id){
+
+        try {
+
+            // obteniendo los objetos
+            await this.getAll()
+                .then( async (res) => {
+
+                    if(res.find(element => element.id === id)){
+
+                    const index = res.findIndex(ele => ele.id === id);
+                    const indice = index;
+
+                    res.splice(indice, 1);
+                    await fs.promises.writeFile(`${this.path}`, JSON.stringify(res, null, 2));
+                    console.log('Producto eliminado exitosamente!');
+
+                    } else {
+                        console.log(`El producto con el id: ${id} no fue encontrado.`);
+                    }
+
+                }).catch((err) => {
+                    console.log(`Ha ocurirdo un error al obtener la data: ${err}.`);
+                })
+
+        } catch (error) {
+            console.log(`Error en el servidor: ${error}`);
+        }
+    }
 }
 
 // exportando la clase para usarlo en server.js
--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-const router = express.Router();
+const routeProduct = express.Router();
+const routeCart = express.Router();
 
 // condiguración de ejs
 const ejs = require('ejs');
-const { join } = require('path');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -21,17 +21,8 @@ const Products = require('./Products.js');
 const product = new Products('productos.json');
 
 
-// saber si el usuario es damin o user
+// saber si el usuario es damin o user, si es false no se mostrara el formulario de registrar productos
 const admin = true;
-
-
-/*=================
-SOCKET.IO SET UP
-=================*/
-/* 
-const srv = require('http').Server(app);
-const io = require('socket.io')(srv);
-*/
 
 
 
@@ -45,6 +36,8 @@ const io = require('socket.io')(srv);
 /*=================
 RUTAS
 =================*/
+
+/* RUTAS PRODUCTOS */
 
 // obteniendo todos los productos
 app.get('/api/productos', async(req, res) => {
@@ -62,22 +55,24 @@ app.get('/api/productos', async(req, res) => {
 
 
 // obteniendo producto por id
-app.get('/api/productos/:id', async(req, res) => {
+routeProduct.get('/:id', async(req, res) => {
     //obteniendo el id
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
 
     try {
         const producto = await product.getById(id);
-        // res.render('./partials/productById.ejs', { product: producto });
-        res.json({ product: producto });
 
-
+        // se verifica si el return de product es un objeto, para aí renderizar el layout que muestra el producto
+        if ( typeof(producto) === 'object') {
+            res.render('./pages/productById.ejs', { product: producto });
+        } else {
+            res.json({ error: 'El id ingresado no existe.' })
+        }
 
     } catch (error) {
         res.json({ error: `Ha ocurrido un error al otener el productos: ${error}.` })
     }
-})
-
+});
 
 
 // añadiendo productos
@@ -96,12 +91,67 @@ app.post('/api/productos', async(req, res) => {
     };
 });
 
+// eliminando producto por su id
+routeProduct.delete('/:id', async (req, res) => {
+
+    // id a eliminar
+    const id = parseInt(req.params.id);
+
+    try {
+        // const deleteProduct = await product.deleteById(id);
+        await product.deleteById(id);
+        res.send('Producto eliminado exitosamente!');
+
+    } catch (error) {
+        res.json({ error: `Ha ocurrido y no se pudo encontrar el id de producto: ${error}` });
+    }
+});
+
+
+// actualizando un elemento por su id
+routeProduct.put('/:id', async(req, res) => {
+
+    // obteniendo data y id
+    let body = req.body;
+    let id = parseInt(req.params.id);
+
+    try {
+        
+        let allProducts = await product.getAll();
+        let findProduct = allProducts.find(ele => ele.id === id);
+
+        let updateProduct = { ...findProduct, ...body };
+        await product.deleteById(id);
+        await product.save(updateProduct);
+
+        res.send('Producto actualizado exitosamente!');
+
+    } catch (error) {
+        res.json({ error: `Ha ocurrido y no se pudo encontrar el id de producto: ${error}` });
+    }
+});
+
+// accedieno añadieno sub rutas a la ruta principal de productos
+app.use('/api/productos', routeProduct);
 
 
 
 
 
 
+// en caso de que la ruta no exista
+app.get('*', (req, res) => {
+    res.json({ error: 'La ruta a la que intenta acceder no existe.' })
+});
+app.post('*', (req, res) => {
+    res.json({ error: 'La ruta a la que intenta acceder no existe.' })
+});
+app.put('*', (req, res) => {
+    res.json({ error: 'La ruta a la que intenta acceder no existe.' })
+});
+app.delete('*', (req, res) => {
+    res.json({ error: 'La ruta a la que intenta acceder no existe.' })
+});
 
 
 // iniciando server y mapeo de errores
